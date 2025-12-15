db710d7148e84fe9bf549740a58a656d1619a1d5
Shahd Hani <hrd9021@gmail.com>
2025-12-15 02:19:55 +0200

    Add files via upload

---
+++ 14-Temporary Tables (Soul).sql
@@ -0,0 +1,25 @@
+-- Temporary Table --
+
+create temporary table Temp_Table
+(First_name varchar(50),
+Last_name varchar(50),
+Favorite_movie varchar(100),
+age int
+);
+
+select*
+from temp_table;
+
+insert into temp_table
+values ("Shahd","Hani","To all boys i have loved before","22");
+
+select*
+from temp_table ;
+
+create temporary table salary_over_50k
+select*
+from employee_salary
+where salary>= 50000;
+
+select*
+from employee_salary;
